2ab0e8c2980cb1b64ed353f4e008c848f8f49a19
koty <kouichi.nishizawa+github@gmail.com>
2015-05-14 11:20:33 +0900

    add chikuma hall

--- hokuto.py
+++ hokuto.py
@@ -11,7 +11,55 @@ def hello_world():
     return 'Hello World!'
 
 
-def _parse_program(url, program_list, room_name):
+def _parse_program_chikuma(program_list):
+    d = pq('http://www.chikuma-bunka.jp/moyoshi.html')
+    prog_by_months = d(u'table :contains("催しもの名・会場")')
+    if not prog_by_months:
+        return program_list
+    for prog in prog_by_months:
+        table = prog.getparent().getparent().getparent()
+        row_span = 0
+        for tr in table.getchildren():
+            if tr.getchildren()[1].getchildren()[0].text == u'日':
+                continue
+            if tr.getchildren()[1].attrib.get('rowspan'):
+                row_span = int(tr.getchildren()[1].attrib.get('rowspan'))
+            date = tr.getchildren()[1].getchildren()[0].text
+            if row_span > 0 and not tr.getchildren()[1].attrib.get('rowspan'):
+                offset = 2
+            else:
+                offset = 0
+            row_span -= 1
+            time_from = tr.getchildren()[4 - offset].getchildren()[0].text
+            time_to = ''
+            subject = tr.getchildren()[3 - offset].getchildren()[0].text
+            room_name = u'更植文化会館' \
+                if _get_hall_name_image_tag(tr.getchildren()[3 - offset]).attrib['src'] == 'image33.gif'\
+                else u'上山田文化会館'
+            program_list.append(
+                {
+                    'date': date,
+                    'time_from': time_from,
+                    'time_to': time_to,
+                    'subject': subject,
+                    'url': 'http://www.chikuma-bunka.jp/moyoshi.html',
+                    'room_name': room_name,
+                    }
+            )
+    return program_list
+
+
+def _get_hall_name_image_tag(td):
+    imgs = [tag for tag in td.getchildren() if tag.tag == 'img']
+    if imgs:
+        return imgs[0]
+    fonts = [tag for tag in td.getchildren() if tag.tag == 'font']
+    font = fonts[0]
+    imgs = [tag for tag in font.getchildren() if tag.tag == 'img']
+    return imgs[0]
+
+
+def _parse_program_hokuto(url, program_list, room_name):
     d = pq(url)
     if not d('#tbl_list tr').children():
         return program_list
@@ -45,16 +93,33 @@ def _parse_program(url, program_list, room_name):
     return program_list
 
 
-@app.route('/hokuto.json')
-def get_program():
+@app.route('/chikuma.json')
+def get_program_chikuma():
+    program_list = []
     try:
-        program_list = []
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat66/', program_list, '大ホール')
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat66/?page=2', program_list, '大ホール')
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat67/', program_list, '中ホール')
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat67/?page=2', program_list, '中ホール')
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat68/', program_list, '小ホール')
-        program_list = _parse_program('http://www.n-bunka.jp/schedule/cat68/?page=2', program_list, '小ホール')
+        program_list = _parse_program_chikuma(program_list)
+    except Exception as e:
+        print(e)
+        raise
+
+    data = {'results': program_list}
+    callback = request.args.get("callback")
+    if callback:
+        return jsonp(data, callback)
+    return jsonify(data)
+
+
+@app.route('/hokuto.json')
+def get_program_hokuto():
+    program_list = []
+    try:
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat66/', program_list, '大ホール')
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat66/?page=2', program_list, '大ホール')
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat67/', program_list, '中ホール')
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat67/?page=2', program_list, '中ホール')
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat68/', program_list, '小ホール')
+        program_list = _parse_program_hokuto('http://www.n-bunka.jp/schedule/cat68/?page=2', program_list, '小ホール')
+        program_list = _parse_program_chikuma(program_list)
     except Exception as e:
         print(e)
 
@@ -66,15 +131,15 @@ def get_program():
 
 
 def jsonp(data, callback="function"):
-    '''
+    """
     http://d.hatena.ne.jp/mizchi/20110124/1295883476
-    :param data: 
-    :param callback: 
+    :param data:
+    :param callback:
     :return:
-    '''
+    """
     return Response("%s(%s);" % (callback, json.dumps(data))
                     , mimetype="text/javascript")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
